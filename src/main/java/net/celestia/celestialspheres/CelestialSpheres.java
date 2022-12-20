1fcd0c1906ea63a7074d27ccd12f1356de6296d8
AstroChara <jilithespaceboy23@gmail.com>
2022-12-20 13:55:31 -0800

    2022-12-20, First Items

--- src/main/java/net/celestia/celestialspheres/CelestialSpheres.java
+++ src/main/java/net/celestia/celestialspheres/CelestialSpheres.java
@@ -1,5 +1,6 @@
 package net.celestia.celestialspheres;
 
+import net.celestia.celestialspheres.item.ModItems;
 import net.fabricmc.api.ModInitializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -10,6 +11,7 @@ public class CelestialSpheres implements ModInitializer {
 
 	@Override
 	public void onInitialize() {
-		
+
+		ModItems.registerModItems();
 	}
 }
---
+++ src/main/java/net/celestia/celestialspheres/item/ModItemGroup.java
@@ -0,0 +1,18 @@
+package net.celestia.celestialspheres.item;
+
+import net.celestia.celestialspheres.CelestialSpheres;
+import net.fabricmc.fabric.api.itemgroup.v1.FabricItemGroup;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.Identifier;
+
+public class ModItemGroup {
+    public static final ItemGroup CELESTIA_MISC = FabricItemGroup.builder(
+        new Identifier(CelestialSpheres.MOD_ID, "celestia-misc"))
+        .icon(() -> new ItemStack(ModItems.TELESCOPE))
+        .build();
+    public static final ItemGroup CELESTIA_MOON = FabricItemGroup.builder(
+        new Identifier(CelestialSpheres.MOD_ID, "celestia-moon"))
+        .icon(() -> new ItemStack(ModItems.TELESCOPE))
+        .build();
+}
