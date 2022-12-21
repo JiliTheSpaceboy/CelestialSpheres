9a4eccf4a467c1a5d7b025d063a5cc027260ca82
AstroChara <jilithespaceboy23@gmail.com>
2022-12-21 01:35:54 -0800

    Add files via upload

---
+++ celestialspheres/CelestialSpheresClient.java
@@ -0,0 +1,10 @@
+package net.celestia.celestialspheres;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class CelestialSpheresClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+        
+    }
+}
