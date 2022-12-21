0f0146c087430bc3b0434a285c783ceb4c6bb29c
AstroChara <jilithespaceboy23@gmail.com>
2022-12-21 07:44:26 -0800

    Add files via upload

--- src/main/java/net/celestia/celestialspheres/item/ModItemGroup.java
+++ src/main/java/net/celestia/celestialspheres/item/ModItemGroup.java
@@ -8,6 +8,10 @@ import net.minecraft.item.ItemStack;
 import net.minecraft.util.Identifier;
 
 public class ModItemGroup {
+    public static final ItemGroup CELESTIA_GENERIC_BLOCKS = FabricItemGroup.builder(
+        new Identifier(CelestialSpheres.MOD_ID, "celestia-generic-blocks"))
+        .icon(() -> new ItemStack(ModBlocks.ANORTHOSITE))
+        .build();
     public static final ItemGroup CELESTIA_MOON = FabricItemGroup.builder(
         new Identifier(CelestialSpheres.MOD_ID, "celestia-moon"))
         .icon(() -> new ItemStack(ModBlocks.LIGHT_LUNAR_REGOLITH))
